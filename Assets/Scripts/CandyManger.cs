ffebce141ba033396a86c7e9c27c933671c05541
Luciva33 <luciva0303@gmail.com>
2024-05-16 16:20:14 +0900

    count

--- Assets/Scripts/CandyManger.cs
+++ Assets/Scripts/CandyManger.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class CandyManger : MonoBehaviour
 {
     const int DefaultCandyAmount = 30;
+    const int Recoveryseconds = 10;
 
     //現在のキャンディのストック数
     public int candy = DefaultCandyAmount;
+    // ストック回数までの残り秒数
+    int counter;
 
     public void ConsumeCandy()
     {
@@ -28,7 +31,32 @@ public class CandyManger : MonoBehaviour
         GUI.color = Color.black;
         //キャンディのストック表示
         string label = "Candy : " + candy;
+        //回復カウントしている時だけ秒数を表示
+        if (counter > 0) label = label + "(" + counter + "s)";
         GUI.Label(new Rect(50, 50, 100, 30), label);
+    }
+
+    void Update()
+    {
+        //キャンディのストックがデフォルトより少なく、
+        //回復カウントをしていないときにカウントをスタートさせる
+        if (candy < DefaultCandyAmount && counter <= 0)
+        {
+            StartCoroutine(RecoverCandy());
+        }
+    }
+
+    IEnumerator RecoverCandy()
+    {
+        counter = Recoveryseconds;
+        //1秒ずつカウントを進める
+        while (counter > 0)
+        {
+            yield return new WaitForSeconds(1.0f);
+            counter--;
+        }
+
+        candy++;
     }
 
 
